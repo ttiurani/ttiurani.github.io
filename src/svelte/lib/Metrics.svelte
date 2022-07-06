7f8e356e878c9784693cac8232bf65f4b0f6908d
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2022-07-06 12:18:39 +0300

    A little styling to stats

--- src/svelte/lib/Metrics.svelte
+++ src/svelte/lib/Metrics.svelte
@@ -4,16 +4,55 @@
 
 <section id="metrics">
 	{#each $dayRanges as dayRange}
-    <div>
-        <h2>{dayRange.start} &ndash; {dayRange.end}</h2>
-        <ul>
-        {#each dayRange.entries as entry}
-            <li>{entry.path} {entry.status} {entry.count}</li>
-        {/each}
-        </ul>
+    <div class="day-range">
+        <table>
+            <caption><h2>{dayRange.start} &ndash; {dayRange.end}</h2></caption>
+            <thead>
+                <tr>
+                    <th>Path</th>
+                    <th class="row-status">Status</th>
+                    <th class="row-hits">Hits</th>
+                </tr>
+            </thead>
+            <tbody>
+            {#each dayRange.entries as entry}
+                <tr>
+                    <td>{entry.path}</td>
+                    <td class="row-status">{entry.status}</td>
+                    <td class="row-hits">{entry.count}</td>
+                </tr>
+            {/each}
+            </tbody>
+        </table>
     </div>
     {/each}
 </section>
 
 <style>
+    table {
+        width: 100%;
+        text-align: left;
+        max-width: 700px;
+    }
+    table caption {
+        text-align: left;
+    }
+    table, thead, tbody, tr, td, th {
+        border-collapse: collapse;
+    }
+    thead {
+        box-shadow: inset 0 -2px;
+    }
+    th, td {
+        padding: 0.5rem 1rem;
+    }
+    .day-range {
+        padding-bottom: 1rem;
+    }
+    .row-status {
+        width: 4rem;
+    }
+    .row-hits {
+        width: 6rem;
+    }
 </style>
