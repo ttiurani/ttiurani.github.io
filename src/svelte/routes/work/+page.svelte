397dd3ccbab72883fe5080b315876dde8e80b237
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2023-04-22 17:02:29 +0300

    Somewhat working quadrant chart

--- src/svelte/routes/work/+page.svelte
+++ src/svelte/routes/work/+page.svelte
@@ -31,20 +31,34 @@
         <p>
             <figure class="quadrant-chart" style="--widget-size: 100%;">
                 <div class="quadrant-chart__y-label">
-                    y label text
+                    <span class="smallish">Conviction required</span>
                 </div>
                 <div class="quadrant-chart__y-arrow"/>
                 <div class="quadrant-chart__sidebar-left">
-                    left sidebar text
+                    <div class="quadrant-chart__sidebar-left--4">
+                        <span class="smallish">Brute optimism</span>
+                    </div>
+                    <div class="quadrant-chart__sidebar-left--12">
+                        <span class="smallish">Grounded optimism</span>
+                    </div>
                 </div>
                 <div class="quadrant-chart__content">
+                    <div class="quadrant-chart__point quadrant-chart__point--5-3"><span>Climate denial</span></div>
+                    <div class="quadrant-chart__point quadrant-chart__point--5-6"><span>Climate delay</span></div>
+                    <div class="quadrant-chart__point quadrant-chart__point--13-3"><span>Activist burnout</span></div>
+                    <div class="quadrant-chart__point quadrant-chart__point--13-15"><span>Climate justice</span></div>
                 </div>
                 <div class="quadrant-chart__x-arrow"/>
                 <div class="quadrant-chart__x-label">
-                    x label  text
+                    <span class="smallish">Effort required</span>
                 </div>
                 <div class="quadrant-chart__content-footer">
-                    bottom sidebar text
+                    <div class="quadrant-chart__content-footer--5">
+                        <span class="smallish">Pacifying optimism</span>
+                    </div>
+                    <div class="quadrant-chart__content-footer--13">
+                        <span class="smallish">Empowering optimism</span>
+                    </div>
                 </div>
             </figure>
         <p>
@@ -52,73 +66,210 @@
 </section>
 
 <style>
+
+:root {
+    --left-sidebar-column: minmax(4%, 1em);
+    --container-content-cell: minmax(4%, 0.85em);
+    --content-cell: minmax(5%, 0.85em);
+    --footer-row: minmax(4%, 1em);
+    --right-label: minmax(4%, 1em);
+    --x-label-padding-right: 0.7em;
+    --content-point-font-size: calc(0.8rem + 0.5vw);
+}
+@media (min-width: 35rem) {
+    :root {
+        --container-content-cell: minmax(5%, 1em);
+        --content-cell: minmax(6%, 1em);
+        --x-label-padding-right: 0.5em;
+        --content-point-font-size: inherit;
+    }
+}
+
 .quadrant-chart {
   height: var(--widget-size);
   width: var(--widget-size);
   margin: 0;
   display: grid;
-  grid-template-columns: repeat(17, minmax(10px, auto)) 10px minmax(10px, auto);
-  grid-template-rows: minmax(10px, auto) 10px repeat(17, minmax(10px, auto));
+  position: relative;
+  grid-template-columns: repeat(2, var(--left-sidebar-column)) repeat(16, var(--container-content-cell)) 1px var(--right-label);
+  grid-template-rows: minmax(10px, auto) 1px repeat(16, var(--container-content-cell)) var(--footer-row);
+  padding-top: 2em;
+  padding-bottom: 2em;
+  border-top: 1px dotted;
+  border-bottom: 1px dotted;
 }
 
 .quadrant-chart__y-label {
-  grid-column: 2 / span 17;
+  grid-column: 2 / span 3;
   grid-row: 1 / span 1;
 }
 
 .quadrant-chart__y-arrow {
   grid-column: 3 / span 1;
   grid-row: 2 / span 1;
+  box-sizing: border-box;
+  position: absolute;
+  left: -0.5em;
+  display: block;
+  transform: scale(var(--ggs,1));
+  width: 22px;
+  height: 22px
 }
 
+.quadrant-chart__y-arrow::after,
 .quadrant-chart__y-arrow::before {
-  content: '^';
+    content: "";
+    display: block;
+    box-sizing: border-box;
+    position: absolute;
+    top: 4px
+}
+    
+.quadrant-chart__y-arrow::after {
+    width: 0.5em;
+    height: 0.5em;
+    border-top: 2px solid;
+    border-left: 2px solid;
+    transform: rotate(45deg);
+    left: 0.27em;
+}
+    
+.quadrant-chart__y-arrow::before {
+    width: 1px;
+    left: 0.5em;
+    height: 10px;
+    background: currentColor
 }
 
 .quadrant-chart__sidebar-left {
   grid-column: 1 / span 2;
   grid-row: 3 / span 17;
+  display: grid;
+  grid-template-columns: repeat(2, var(--left-sidebar-column));
+  grid-template-rows: repeat(16, var(--container-content-cell)) var(--footer-row);
 }
 
-.quadrant-chart__x-arrow {
-  grid-column: 3 / span 1;
-  grid-row: 2 / span 1;
+.quadrant-chart__sidebar-left span {
+  writing-mode: vertical-rl;
+  transform:scale(-1);
+}
+
+.quadrant-chart__sidebar-left--4 {
+  grid-column: 1 / span 2;
+  grid-row: 4 / span 1;
+}
+
+.quadrant-chart__sidebar-left--12 {
+  grid-column: 1 / span 2;
+  grid-row: 12 / span 1;
 }
 
 .quadrant-chart__x-arrow {
   grid-column: 19 / span 1;
   grid-row: 18 / span 1;
+
+  box-sizing: border-box;
+  position: absolute;
+  display: block;
+  transform: scale(var(--ggs,1));
+  border-top: 3px solid transparent;
+  border-bottom: 2px solid transparent;
+  box-shadow: inset 0 0 0 1px;
+  width: 24px;
+  height: 6px;
+  bottom: -2px;
+  left: -1em;
 }
 
-.quadrant-chart__x-arrow::before {
-  content: '>';
+.quadrant-chart__x-arrow::after {
+    content: "";
+    display: block;
+    box-sizing: border-box;
+    position: absolute;
+    width: 0.5em;
+    height: 0.5em;
+    border-top: 2px solid;
+    border-right: 2px solid;
+    transform: rotate(45deg);
+    right: 0;
+    bottom: -0.22em;
 }
 
 .quadrant-chart__x-label {
   grid-column: 20 / span 1;
   grid-row: 18 / span 2;
+  padding-left: var(--x-label-padding-right)
 }
 
 .quadrant-chart__content-footer {
   padding-right: 1em;
   grid-area: footer;
-  grid-column: 3 / span 17;
+  grid-column: 3 / span 16;
   grid-row: 19 / span 1;
+  display: grid;
+  grid-template-columns: repeat(16, var(--content-cell));
+  grid-template-rows: var(--footer-row);
+}
+
+.quadrant-chart__content-footer--5 {
+  grid-column: 5 / span 2;
+  grid-row: 1 / span 1;
+}
+
+.quadrant-chart__content-footer--13 {
+  grid-column: 13 / span 2;
+  grid-row: 1 / span 1;
 }
  
-/* The content area */
+ 
 .quadrant-chart__content {
   grid-column: 3 / span 16;
   grid-row: 3 / span 16;
-  /* Remove any padding so we have as much space to work with inside the element */
   padding: 0;
   position: relative;
-  /* The chart borders */
   border-bottom: 1px solid;
   border-left: 1px solid;
-  /* https://css.gg/arrow-up
-     https://css.gg/arrow-long-right
-   */
+  display: grid;
+  grid-template-columns: repeat(16, var(--content-cell));
+  grid-template-rows: repeat(16, var(--content-cell));
+}
+
+.quadrant-chart__point {
+  position: relative;
+}
+
+.quadrant-chart__point:before {
+  content: '';
+  display: block;
+  padding-top: 100%;
+}
+
+.quadrant-chart__point span {
+  position: absolute;
+  top: 0; left: 0;
+  height: 100%;
+  width: 100%;
+  font-size: var(--content-point-font-size);
+}
+
+.quadrant-chart__point--5-3 {
+  grid-column: 5 / span 1;
+  grid-row: 3 / span 1;
+}
+
+.quadrant-chart__point--5-6 {
+  grid-column: 5 / span 1;
+  grid-row: 6 / span 1;
+}
+
+.quadrant-chart__point--13-3 {
+  grid-column: 13 / span 1;
+  grid-row: 3 / span 1;
+}
+
+.quadrant-chart__point--13-15 {
+  grid-column: 13 / span 1;
+  grid-row: 14 / span 1;
 }
 </style>
 
