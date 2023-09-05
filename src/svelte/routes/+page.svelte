d77a5ead93e92c929ddea6870971ba8d7d5160d9
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2023-09-05 09:54:46 +0300

    Use working feed provider

--- src/svelte/routes/+page.svelte
+++ src/svelte/routes/+page.svelte
@@ -96,7 +96,7 @@
                 sandbox="allow-top-navigation allow-scripts"
                 width={!layoutWidth || layoutWidth > 700 ? 700 : layoutWidth}
                 height="800"
-                src="https://fedifeed.com/api/v1/feed?user=ttiurani&instance=https%3A%2F%2Ffosstodon.org&instance_type=&theme=masto-dark&size=100&header=false&replies=false&boosts=false"
+                src="https://www.mastofeed.com/apiv2/feed?userurl=https%3A%2F%2Ffosstodon.org%2Fusers%2Fttiurani&theme=auto&size=100&header=false&replies=false&boosts=false"
             />
         </p>
     </Content>
